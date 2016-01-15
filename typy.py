020bb14f57e4c4dbd316a10c82e22f4b06537eca
EwilDawe <ewildawe@gmail.com>
2016-01-15 22:46:43 +0300

    Added basic functionality

--- typy.py
+++ typy.py
@@ -14,4 +14,19 @@ def hold(*keys):
 def release(*keys):
 	for key in keys:
 		win32api.keybd_event(config.code[key], 0, win32con.KEYEVENTF_KEYUP, 0)
-
+		
+def type(s = ''):
+	for char in s:
+		if char.isupper():		#Handles uppercase
+			hold('shift', char.lower())
+		elif char == " ":		#Handles spaces
+			press('spacebar')
+		elif char == "\n":		#Handles newline
+			press('enter')
+		elif char in (')','!','@','#','$','%','^','&','*','('): 	#Handles shift & number
+			hold('shift', str((')','!','@','#','$','%','^','&','*','(').index(char)))
+		elif char in ('{', '}', '<', '>', '?', ':', '"', '_', '=', '~'):
+			hold('shift', ('[', ']', ',', '.', '/', ';', "'", '-', '+', '`')[('{', '}', '<', '>', '?', ':', '"', '_', '=', '~').index(char)])
+		else:
+			press(char)
+		

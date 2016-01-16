b5d395ba5c25fd5f36865bfd9fc4bcd8aa2cd877
EwilDawe <ewildawe@gmail.com>
2016-01-16 22:26:42 +0300

    Added mouse support, docstrings and updated names

---
+++ mousepy.py
@@ -0,0 +1,28 @@
+﻿import win32api, win32con
+
+def moveMouse(x, y): #Jumps to x,y - no fluid motion yet
+	"""
+	Moves the cursor to (x, y)
+	"""
+	
+	win32api.SetCursorPos((x, y))
+	
+def clickLeft(x = win32api.GetCursorPos()[0], y = win32api.GetCursorPos()[1]):
+	'''
+	Simulates a mouse left click on pixel (x,y) if x and y are provided.
+	If x and y are not passed to this function, a mouse click is simulated at the current (x,y)
+	'''
+	
+	moveMouse(x, y)
+	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+	
+def clickRight(x = win32api.GetCursorPos()[0], y = win32api.GetCursorPos()[1]):
+	"""
+	Simulates a mouse right click on pixel (x,y) if x and y are provided.
+	If x and y are not passed to this function, a mouse click is simulated at the current (x,y)
+	"""
+	
+	moveMouse(x, y)
+	win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
+	win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)	
--- typy.py
+++ typy.py
@@ -1,21 +1,40 @@
 ﻿import win32api, win32con
 import config
+import time
 
 def press(*keys):
+	"""
+	Simulates a key-press for all the keys passed to the function.
+	"""
+	
 	for key in keys:
 		win32api.keybd_event(config.code[key], 0, 0, 0)
 		release(key)
 
-def hold(*keys):
+def hold(*keys, time = 0):
+	"""
+	Simulates the holding of all the keys passed to the function.
+	These keys are held down for a default period of 0 seconds before release.
+	"""
+	
 	for key in keys:
 		win32api.keybd_event(config.code[key], 0, 0, 0)
+	time.sleep(time)
 	release(*keys)
 
 def release(*keys):
+	"""
+	Simulates the release of all the keys passed to this function.
+	"""
+	
 	for key in keys:
 		win32api.keybd_event(config.code[key], 0, win32con.KEYEVENTF_KEYUP, 0)
 		
 def type(s = ''):
+	"""
+	Automates the process of typing by converting a string into a set of press() and hold() calls.
+	"""
+	
 	for char in s:
 		if char.isupper():		#Handles uppercase
 			hold('shift', char.lower())

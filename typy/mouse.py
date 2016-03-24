9401e7d3bc58bf81a476e38cfa107823e700d8dd
EwilDawe <ewildawe@gmail.com>
2016-03-25 00:12:35 +0300

    Added move_arc prototype & small bug fixes

--- typy/mouse.py
+++ typy/mouse.py
@@ -1,6 +1,7 @@
 ﻿import win32api
 import win32con
 import time
+import math
 
 
 def move(x, y):
@@ -16,22 +17,47 @@ def move_line(x, y, speed = 1):
     """
 
     _x, _y = win32api.GetCursorPos()
-    if not (x - _x):  # make sure difference between x ordinates is not 0, as the gradient would be undefined
+    if x - _x:
         m = (y - _y) / (x - _x)
         c = y - (m * x)
         for a in range(_x, x + 1, speed) if _x < x else range(_x, x - 1, -speed):
             b = int(m * a + c)
             move(a, b)
             time.sleep(0.01)
-        move(x, y)
     else:
         for b in range(_y, y + 1, speed) if _y <= y else range(_y, y - 1, -speed):
             move(x, b)
             time.sleep(0.01)
-        move(x, y)
+    move(x, y)
+
+
+def move_arc(x, y, r, speed = 1, orientation = True):
+    # WARNING: This function currently contains inaccuracy likely due to the rounding of trigonometric functions
+    """
+    Moves the cursor in an arc of radius r to (x, y) at a certain speed.
+    """
+
+    _x, _y = win32api.GetCursorPos()
+    c_len = (r**2 - (((x - _x)/2)**2 + ((y - _y)/2)**2))**0.5
+    t = (c_len**2/((y - _y)**2 + (x - _x)**2))**0.5
+    t = t if orientation else -t
+    centre = ((_x + x)/2 + t*(_x - x), (_y + y)/2 + t*(y - _y))
+    if any(isinstance(ordinate, complex) for ordinate in centre):
+        raise ValueError("Radius too low - minimum: {}".format(((x - _x)**2 + (y - _y)**2)**0.5/2))
+    theta = math.atan2(_y - centre[1], _x - centre[0])
+    end = math.atan2(y - centre[1], x - centre[0])
+    while theta < end:
+        move(*list(map(round, (centre[0] + r*math.cos(theta), centre[1] + r*math.sin(theta)))))
+        theta += 0.01
+        time.sleep(0.01)
+    move(x, y)
 
 
 def drag(*args, function = move):
+    """
+    Drags the mouse along a specified path
+    """
+
     x, y = win32api.GetCursorPos()
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
     function(*args)
@@ -71,3 +97,5 @@ def click_right(x = None, y = None):
     move(x, y)
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
+
+move_arc(1000, 1000, 800, speed = 1)
